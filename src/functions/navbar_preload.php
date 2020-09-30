865846f72860428e8b2803d24f94f2327e9e74fe
Edison dos Santos <38053585+Edison2ST@users.noreply.github.com>
2020-09-30 16:47:18 -0400

    Añadida la precarga del navbar para usuarios que han ingresado, incluyendo moderadores y administradores

--- src/functions/navbar_preload.php
+++ src/functions/navbar_preload.php
@@ -1,3 +1,5 @@
 <?php
 if ($tipo_navbar === "logout") $navbar = [["descripcion" => "Foros", "estado" => $estado["foro"] ?? 0, "link" => "foros.php"], ["descripcion" => "Iniciar sesión", "estado" => $estado["iniciar_sesion"] ?? 0, "link" => "iniciar_sesion.php"], ["descripcion" => "Registrarse", "estado" => $estado["registrar"] ?? 0, "link" => "registrar.php"]];
+if ($tipo_navbar === "rango0") $navbar = [["descripcion" => "Foros", "estado" => $estado["foro"] ?? 0, "link" => "foros.php"], ["descripcion" => "Cerrar sesión", "estado" => $estado["cerrar_sesion"] ?? 0, "link" => "cerrar_sesion.php"]];
+if ($tipo_navbar === "rango1") $navbar = [["descripcion" => "Foros", "estado" => $estado["foro"] ?? 0, "link" => "foros.php"], ["descripcion" => "Rangos", "estado" => $estado["rangos"] ?? 0, "link" => "rangos.php"], ["descripcion" => "Cerrar sesión", "estado" => $estado["cerrar_sesion"] ?? 0, "link" => "cerrar_sesion.php"]];
 ?>
