534fe4e3bb09d0149e01cfd785c2027f993c5bb4
Edison dos Santos <38053585+Edison2ST@users.noreply.github.com>
2020-09-30 16:49:15 -0400

    Añadida una función para comprobar en varias páginas si el usuario está ingresado o no, además de cargar su debida navbar

---
+++ src/functions/checklogin_preload.php
@@ -0,0 +1,26 @@
+<?php
+include "userdata.php";
+session_start();
+$userdata = new UserData();
+if (isset($_SESSION["usuario_id"], $_SESSION["token"]))
+{
+    $resultado = $userdata->establecerToken($_SESSION["usuario_id"], $_SESSION["token"]);
+    if ($resultado === true)
+    {
+        $ingresado = true;
+        $tipo_navbar = $userdata->rango === 0 ? "rango0" : "rango1";
+    }
+    else
+    {
+        unset($_SESSION["usuario_id"]);
+        unset($_SESSION["token"]);
+        $ingresado = false;
+        $tipo_navbar = "logout";
+    }
+}
+else
+{
+    $ingresado = false;
+    $tipo_navbar = "logout";
+}
+?>
